e435dfa107c89016f90c82e4c23cff4df2fb7fdc
2003dinuli <147374633+2003dinuli@users.noreply.github.com>
2024-04-19 21:05:41 +1000

    Add files via upload

---
+++ WriteMultipleFields/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Dinuli"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "2003abcd"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2467186			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "KBNLFYF9RVEHJVMM"   // replace XYZ with your channel write API Key
